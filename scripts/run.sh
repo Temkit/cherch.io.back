8da34552be252358cf8295afc824a4cbe57ba3fb
yet.marketing <temkit.ali@gmail.com>
2020-03-22 14:45:30 +0100

    pm2 config

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,19 @@
 sonic="sonic -c ./config.cfg"
 pid=$(pidof 'sonic')
+express=$(node './../app.js')
 
  if [ -z $pid]
  then
     printf "Starting process '%s' with command '%s'.\n" "sonic" "$sonic"
     $sonic &
+
+     if [ -z $express]
+ then
+    printf "Starting process '%s' with command '%s'.\n" "Node Express" "$express"
+    $express &
+ else
+    printf "Process '%s' is running.\n" "express"
+ fi
  else
     printf "Process '%s' is running.\n" "sonic"
  fi
